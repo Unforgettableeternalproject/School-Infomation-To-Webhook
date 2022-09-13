16ee588983535a1cc5a1aef039f02b8931ad110e
Bernie <ptyc4076@gmail.com>
2022-09-13 13:06:32 +0800

    跟IFTTT連接 9/13

--- 學校網站的爬蟲.py
+++ 學校網站的爬蟲.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 response = requests.get(
-    "https://news.nknu.edu.tw/nknu_News/lastNewsList.aspx?cat=SCHOOLADMIN")
+    "https://news.nknu.edu.tw/nknu_News/")
 soup = BeautifulSoup(response.text, "html.parser")
 result = soup.find_all("td", limit = 3)[1]
 rlist = str(result).split('>')
@@ -10,6 +10,12 @@ temp = website.split("amp;")
 website = ''.join(x for x in temp)
 title = rlist[2][:-3]
 
+evt = "info_updated"
+key = "bgSBNfTuWeTf0X1eoSCPMi"
+val = title
+url = (f'https://maker.ifttt.com/trigger/{evt}' + f'/with/key/{key}?value1={val}')
+r = requests.get(url)
+print(r.text)
 print("Website: " + website + '\n' + "Title: " + title)
 #美化輸出環境
 #架上伺服器(heroku?)
