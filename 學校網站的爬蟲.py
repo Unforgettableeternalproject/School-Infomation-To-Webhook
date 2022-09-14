c5082c87df4aa92e425fffc4ade47854de8d642e
Bernie <ptyc4076@gmail.com>
2022-09-14 17:37:01 +0800

    可能有潛在的問題 9/13

--- 學校網站的爬蟲.py
+++ 學校網站的爬蟲.py
@@ -36,15 +36,15 @@ finalM = f"最新公告! 公告處:{unit}\n->{title}\n\n網站連結:{website}\n
 #print(final_message)
 
 #Chapter 3: Send Into Oblivion
-if(finalM != prevM):
-    webhook = DiscordWebhook(url='https://discordapp.com/api/webhooks/1019081282002632744/aOhQ3FBys64iqsO85c1n4yw8cwSgA5Kl7LWb4qR6jSG6Gk82O-h2djkVnNPDrXHOAi7z', rate_limit_retry=True,
+if(title != prevM):
+    webhook = DiscordWebhook(url='https://discordapp.com/api/webhooks/1019106976409583707/dTM7dWoKUwljjDQKy7xptyk1EKZnVYeXbcEGr9hpUsmw9q_Y6LGegtJYBrQpqQnnTfhb', rate_limit_retry=True,
                             content=finalM)
     response = webhook.execute()
     print("發現更新，已推播至DC伺服器!")
 else:
     print("沒有更新...")
 
-w.write(finalM)
+w.write(title)
 print(f"程式執行完畢，目前時刻:{date}")
 
 #Chapter 4: And it repeats...
